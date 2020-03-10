d21395b685e91dcb96c5596a298d94d288de4bbe
publicwangfei <39934259+publicwangfei@users.noreply.github.com>
2020-03-10 23:40:19 +0800

    Create removeDuplicates.java
    add file

---
+++ removeDuplicates.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        Set<Integer> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        nums = set.stream().mapToInt(Integer::intValue).toArray();
+        return set.size();
+    }
+}
